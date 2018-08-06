11cbf5708ee8c9a1470ff8bd2dfd3f32cc50f8a9
FamEscar <FamEscar@192.168.1.5>
2018-08-05 20:06:11 -0600

    dao cliente, conexion, modelo cliente y bean cliente

---
+++ PruebaJsfPrimeFaces/src/main/java/com/pruebajsf/dao/prueba.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pruebajsf.dao;
+
+import com.pruebajsf.modelo.Cliente;
+
+/**
+ *
+ * @author FamEscar
+ */
+public class prueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       ClienteDao cliente = new ClienteDao();
+       Cliente datos = new Cliente();
+       datos.setId(11);
+       datos.setNombre("inventado");
+       datos.setApellido("nuevo");
+       datos.setNit("123");
+       String mensaje=cliente.registrarCliente(datos);
+        System.out.println(mensaje);
+    }
+    
+}
